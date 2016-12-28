def61ec4fb4b3c9837c8376e237c24a9b0dfdafb
Daniel <Daniel>
2016-12-28 17:33:30 +0100

    Initial version 0.1.0

---
+++ ReverseRotate/control.lua
@@ -0,0 +1,36 @@
+
+local function reverseRotate(event)
+	local player = game.players[event.player_index]
+	
+	if player.cursor_stack.valid_for_read then
+		
+--		local cursor = player.cursor_stack
+--		if cursor.type == 'blueprint' and cursor.is_blueprint_setup() then
+--			
+--			local entities = cursor.get_blueprint_entities()
+--			local tiles = cursor.get_blueprint_tiles()
+--			
+--			TODO: blueprints
+--			
+--		elseif cursor.type ~= 'blueprint' then
+
+		player.rotate_for_build()
+		player.rotate_for_build()
+		player.rotate_for_build()
+
+--		end
+
+	
+	elseif player.selected then
+	
+		local entity = player.selected
+	
+		if entity.rotatable then
+		
+			entity.direction = (entity.direction - 2) % 8
+			
+		end
+	end
+end
+
+script.on_event('ReverseRotate', reverseRotate)
---
+++ ReverseRotate/data.lua
@@ -0,0 +1,7 @@
+data:extend{
+	{
+		type = "custom-input",
+		name = "ReverseRotate",
+		key_sequence = "SHIFT + R"
+	}
+}
